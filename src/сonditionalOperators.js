2081d24095453783b55065e38806852cd51b81b0
Your Name <you@example.com>
2021-11-09 10:05:44 -0500

    add tests

--- src/сonditionalOperators.js
+++ src/сonditionalOperators.js
@@ -1,46 +1,43 @@
-//Если а – четное посчитать а*б, иначе а+б
-//   const firsnNumber = parseInt(prompt("Введите число 1"));
-//   const secondNumber = parseInt(prompt("Введите число 2"));
-//   const result = firsnNumber % secondNumber == 0 ? console.log(firsnNumber * secondNumber) : console.log(firsnNumber + secondNumber);
+// Если а – четное посчитать а*б, иначе а+б
 
-//Определить какой четверти принадлежит точка с координатами (х,у)
+function mult (firstNumber,secondNumber) {
+  const result = firstNumber % secondNumber == 0 ? firstNumber * secondNumber : firstNumber + secondNumber;
+  return result;
+}
+// Определить какой четверти принадлежит точка с координатами (х,у)
 
-// const x = parseInt(prompt("Введите x"));
-// const y = parseInt(prompt("Введите y"));
-
-// if ((x > 0) & (y > 0)) {
-//   console.log("Первая четверть");
-// }
-// if ((x > 0) & (y < 0)) {
-//   console.log("Вторая четверть");
-// }
-// if ((x < 0) & (y < 0)) {
-//   console.log("Третья четверть");
-// }
-// if ((x < 0) & (y > 0)) {
-//   console.log("Четвертая четверть");
-// }
-// if ((x == 0) & (y == 0)) {
-//   console.log("Центр координат");
-// }
+function s (x,y) {
+if ((x > 0) && (y > 0)) {
+  return ("Первая четверть");
+}
+if ((x > 0) && (y < 0)) {
+  return("Вторая четверть");
+}
+if ((x < 0) && (y < 0)) {
+  return("Третья четверть");
+}
+if ((x < 0) && (y > 0)) {
+  return("Четвертая четверть");
+}
+if ((x == 0) &&(y == 0)) {
+  return("Центр координат");
+}
+}
 
 // Найти суммы только положительных из трех чисел
-
-// const firstNumber = parseInt(prompt("Введите число 1"));
-// const secondNumber = parseInt(prompt("Введите число 2"));
-// const thirdNumber = parseInt(prompt("Введите число 3"));
-// let sum;
-// if ((firstNumber >= 0) & (secondNumber >= 0) & (thirdNumber >= 0)) {
-//   sum = firstNumber + secondNumber + thirdNumber;
-// } else if ((firstNumber < 0) & (secondNumber >= 0) & (thirdNumber >= 0)) {
-//   sum = secondNumber + thirdNumber;
-// } else if ((firstNumber >= 0) & (secondNumber < 0) & (thirdNumber >= 0)) {
-//   sum = firstNumber + thirdNumber;
-// } else if ((firstNumber >= 0) & (secondNumber >= 0) & (thirdNumber < 0)) {
-//   sum = firstNumber + secondNumber;
-// }
-// console.log(sum);
-
+function sumOfPosNum(firstNumber,secondNumber,thirdNumber) {
+let sum;
+if ((firstNumber >= 0) & (secondNumber >= 0) & (thirdNumber >= 0)) {
+  sum = firstNumber + secondNumber + thirdNumber;
+} else if ((firstNumber < 0) & (secondNumber >= 0) & (thirdNumber >= 0)) {
+  sum = secondNumber + thirdNumber;
+} else if ((firstNumber >= 0) & (secondNumber < 0) & (thirdNumber >= 0)) {
+  sum = firstNumber + thirdNumber;
+} else if ((firstNumber >= 0) & (secondNumber >= 0) & (thirdNumber < 0)) {
+  sum = firstNumber + secondNumber;
+}
+return sum;
+}
 // Посчитать выражение (макс(а*б*с, а+б+с))+3
 
 // const firstNumber = parseInt(prompt("Введите число 1", ""));
---
+++ tests/сonditionalOperators.spec.js
@@ -0,0 +1,47 @@
+describe('On Multiplication or sum', () => {
+    it('should mult',() => {
+        expect(mult(2, 2)).to.equal(4);
+    });
+
+    it('should sum', () => {
+        expect(mult(1, 5)).to.equal(6);
+    });
+});
+    
+    
+    describe('On chois quarter', () => {
+        it('should return 1 quarter', () => {
+            expect(s(3, 2)).to.equal("Первая четверть");
+        });
+
+        it('should return 2quarter', () => {
+            expect(s(1, -2)).to.equal("Вторая четверть");
+        });
+        it('should return 3 quarter', () => {
+            expect(s(-1, -2)).to.equal("Третья четверть");
+        });
+        it('should return 4 quarter', () => {
+            expect(s(-1, 2)).to.equal("Четвертая четверть");
+        });
+        it('should return cener', () => {
+            expect(s(0, 0)).to.equal("Центр координат");
+        });
+    });
+
+    describe('On chois quarter', () => {
+        it('should sum only pos', () => {
+            expect(sumOfPosNum(3, 2,-2)).to.equal(5);
+        });
+        it('should sum only pos', () => {
+            expect(sumOfPosNum(3, -1, 1)).to.equal(4);
+        });
+        it('should sum only pos', () => {
+            expect(sumOfPosNum(-3, 2,5)).to.equal(7);
+        });
+        it('should sum only pos', () => {
+            expect(sumOfPosNum(-3, -2,5)).to.equal();
+        });
+
+    });
+    
+    
