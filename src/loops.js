84c9db2bafa0a09c6896a9abbdee43f11d6d1921
Vitalii-sh <Vitalik0505350@gmail.com>
2021-11-16 23:58:52 +0200

    add new tests

--- src/loops.js
+++ src/loops.js
@@ -1,77 +1,98 @@
 // Найти сумму четных чисел и их количество в диапазоне от 1 до 99
-// let sum = 0;
-// let count = 0;
-// for (i = 1; i < 99; i++) {
-//   if (i % 2 == 0) {
-//     sum += i;
-//     count++;
-//   }
-// }
-// console.log(sum, count);
+function sumAndCount(value) {
+  let sum = 0;
+  let count = 0;
+  if (value > 0) {
+    for (i = value; i < 99; i++) {
+      if (i % 2 == 0) {
+        sum += i;
+        count++;
+      }
+    }
+  } else {
+    return "start value can't be 0 or less";
+  }
+  return `sum = ${sum}, count = ${count}`;
+}
 
 // Проверить простое ли число? (число называется простым, если оно делится только само на себя и на 1)
-
-// const userNumber = parseInt(prompt("Введите число"));
-// let num = "Это простое число";
-// for (i = 2; i < userNumber; i++) {
-//   if (userNumber % i == 0) {
-//     num = "Это не простое число";
-//   }
-// }
-// console.log(num);
+function isSimpleNumber(userNumber) {
+  let num = "Это простое число";
+  for (i = 2; i < userNumber; i++) {
+    if (userNumber % i == 0) {
+      return (num = "Это не простое число");
+    }
+  }
+  return num;
+}
 
 // Найти корень натурального числа с точностью до целого (рассмотреть вариант последовательного подбора и метод бинарного поиска)
-
-// const userNumber = parseInt(prompt("Введите число"));
-// let isRoot = 1;
-// while (isRoot * isRoot <= userNumber) {
-//   isRoot++;
-// }
-// console.log(isRoot - 1);
-
+function findRoot(value) {
+  let isRoot = 1;
+  if (value >= 0)
+    while (isRoot * isRoot <= value) {
+      isRoot++;
+    }
+  else {
+    return "must be > 0";
+  }
+  return parseInt(isRoot - 1);
+}
 // Метод бинарного поиска
-
-// const userNumber = parseInt(prompt("Введите число"));
-// let isRoot = userNumber;
-// while (isRoot * isRoot > userNumber) {
-//   isRoot /= 2;
-// }
-// while (isRoot * isRoot < userNumber) {
-//   isRoot++;
-// }
-// console.log(isRoot);
+function findRoot2(value) {
+  let isRoot = value;
+  if (value >= 0) {
+    while (isRoot * isRoot > value) {
+      isRoot /= 2;
+    }
+    while (isRoot * isRoot < value) {
+      isRoot++;
+    }
+  } else {
+    return "must be > 0";
+  }
+  return parseInt(isRoot);
+}
 
 // Вычислить факториал числа n. n! = 1*2*…*n-1*n;
-
-// const userNumber = parseInt(prompt("Введите число"));
-// let factorial = 1;
-// if (userNumber > 1) {
-//   for (i = 2; i <= userNumber; i++) {
-//     factorial *= i;
-//   }
-// }
-// console.log(factorial);
-
+function findFactorial(value) {
+  let factorial = 1;
+  if (value > 1) {
+    for (i = 2; i <= value; i++) {
+      factorial *= i;
+    }
+  } else {
+    return "value must be > 1";
+  }
+  return parseInt(factorial);
+}
 // Посчитать сумму цифр заданного числа
-
-// let userNumber = prompt("Введите число");
-// let sum = 0;
-// let tmp;
-// while (userNumber != 0) {
-//   tmp = userNumber % 10;
-//   userNumber = (userNumber - tmp) / 10;
-//   sum += tmp;
-// }
-// console.log(sum);
-
+function sumOfNum(value) {
+  let sum = 0;
+  let tmp;
+  if (value > 0) {
+    while (value != 0) {
+      tmp = value % 10;
+      value = (value - tmp) / 10;
+      sum += tmp;
+    }
+  } else {
+    return "value must be > 1";
+  }
+  return sum;
+}
 // Вывести число, которое является зеркальным отображением последовательности цифр заданного числа, например, задано число 123, вывести 321.
-// let userNumber = prompt("Введите число");
-// numbers = new String(userNumber);
-// reversNumber = new String();
-// for (let i = numbers.length; i >= 0; i--) {
-//   reversNumber += numbers.charAt(i);
-// }
-// console.log(reversNumber);
-
+function reversNumbers(value) {
+  numbers = new String(value);
+  reversNumber = new String();
+  if (value >= 10) {
+    for (let i = numbers.length; i >= 0; i--) {
+      reversNumber += numbers.charAt(i);
+    }
+  } else {
+    return "value must be >= 10";
+  }
+  return reversNumber;
+}
 //Or
 // console.log(userNumber.split("").reverse().join(""));
--- src/simpleArray.js
+++ src/simpleArray.js
@@ -1,134 +1,134 @@
 // Найти минимальный элемент массива
-// const arr = prompt("Введите массив чисел");
-// let minElement = arr[0];
-// for (i = 1; i < arr.length; i++) {
-//   if (arr[i] < minElement) {
-//     minElement = arr[i];
-//   }
-// }
-// console.log(minElement);
+function minElementOfArr(value) {
+  let minElement = value[0];
+  for (i = 1; i < value.length; i++) {
+    if (value[i] < minElement) {
+      minElement = value[i];
+    }
+  }
+  return minElement;
+}
 
-// Найти максимальный элемент массива
-// const arr = prompt("Введите массив чисел");
-// let maxElement = arr[0];
-// for (i = 1; i < arr.length; i++) {
-//   if (arr[i] > maxElement) {
-//     maxElement = arr[i];
-//   }
-// }
-// console.log(maxElement);
+// Найти максимальный элемент
 
+function maxElementOfArr(value) {
+  let maxElement = value[0];
+  for (i = 1; i < value.length; i++) {
+    if (value[i] > maxElement) {
+      maxElement = value[i];
+    }
+  }
+  return maxElement;
+}
 // Найти индекс минимального элемента массива
-// const arr = prompt("Введите массив чисел");
-// let index = 0;
-// let minValue = arr[0];
-// for (i = 1; i < arr.length; i++) {
-//   if (arr[i] < minValue) {
-//     index = i;
-//     minValue = arr[i];
-//   }
-// }
-// console.log(index);
-
+function findMinIndexOfArr(value) {
+  let index = 0;
+  let minValue = value[0];
+  for (i = 1; i < value.length; i++) {
+    if (value[i] < minValue) {
+      index = i;
+      minValue = value[i];
+    }
+  }
+  return index;
+}
 // Найти индекс максимального элемента массива
-// const arr = prompt("Введите массив чисел");
-// let index = 0,
-//   maxValue = arr[0];
-// for (i = 1; i < arr.length; i++) {
-//   if (arr[i] > maxValue) {
-//     index = i;
-//     maxValue = arr[i];
-//   }
-// }
-// console.log(index);
-
+function findMaxIndexOfArr(value) {
+  let index = 0,
+    maxValue = value[0];
+  for (i = 1; i < value.length; i++) {
+    if (value[i] > maxValue) {
+      index = i;
+      maxValue = value[i];
+    }
+  }
+  return index;
+}
 // Посчитать сумму элементов массива с нечетными индексами
-// const userArr = [3, 3, 3, 3, 3, 3];
-// let sum = 0;
-// for (let i = 0; i <= arr.length; i++) {
-//   if (i % 2 == 1) {
-//     sum = sum + arr[i];
-//   }
-// }
-// console.log(sum);
-
+function sumOfOddIndexes(value) {
+  let sum = 0;
+  for (let i = 0; i <= value.length; i++) {
+    if (i % 2 == 1) {
+      sum = sum + value[i];
+    }
+  }
+  return sum;
+}
 //Сделать реверс массива (массив в обратном направлении)
-// const userArr = prompt("Введите массив");
-// let reverseArr = [];
-// numbers = new String(userArr);
-// for (let i = numbers.length - 1; i >= 0; i--) {
-//   reverseArr += numbers[i];
-// }
-// console.log(reverseArr);
-
+function reverseArr(value) {
+  let reverseArr = [];
+  numbers = new String(value);
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    reverseArr += numbers[i];
+  }
+  return reverseArr;
+}
 // Посчитать количество нечетных элементов массива
-
-// const arr = [1, 2, 3, 4, 5, 6];
-// let sumOdd = 0;
-// let sumPar = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] % 2 === 0) {
-//     sumPar++;
-//   } else {
-//     sumOdd++;
-//   }
-// }
-// console.log("Odd : " + sumOdd);
-// console.log("Par : " + sumPar);
-
+function numOfOddElements(value) {
+  let sumOdd = 0;
+  let sumPar = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] % 2 === 0) {
+      sumPar++;
+    } else {
+      sumOdd++;
+    }
+  }
+  return sumOdd;
+}
 // Поменять местами первую и вторую половину массива, например, для массива 1 2 3 4, результат 3 4 1 2
-// const arr = [1, 2, 3, 4];
-// const half = arr.length / 2;
-// const div = half + (arr.length % 2);
-// for (i = 0; i < div; i++) {
-//   let currentElement = arr[i];
-//   arr[i] = arr[div + i];
-//   arr[div + i] = currentElement;
-// }
-// console.log(arr);
-
+function swapElemOfArr(value) {
+  const half = value.length / 2;
+  const div = half + (value.length % 2);
+  for (i = 0; i < div; i++) {
+    let currentElement = value[i];
+    value[i] = value[div + i];
+    value[div + i] = currentElement;
+  }
+  return value;
+}
 // Отсортировать массив (пузырьком (Bubble), выбором (Select), вставками (Insert))
-// const arr = [1, 3, 5, 2, 4];
-// {
-//   for (lengthIn = arr.length; lengthIn > 0; lengthIn--) {
-//     for (currSwap = 0; currSwap < lengthIn - 1; currSwap++) {
-//       if (arr[currSwap] > arr[currSwap + 1]) {
-//         let curElement = arr[currSwap];
-//         arr[currSwap] = arr[currSwap + 1];
-//         arr[currSwap + 1] = curElement;
-//       }
-//     }
-//   }
-//   console.log(arr);
-// }
+function bubble(value) {
+  for (lengthIn = value.length; lengthIn > 0; lengthIn--) {
+    for (currSwap = 0; currSwap < lengthIn - 1; currSwap++) {
+      if (value[currSwap] > value[currSwap + 1]) {
+        let curElement = value[currSwap];
+        value[currSwap] = value[currSwap + 1];
+        value[currSwap + 1] = curElement;
+      }
+    }
+  }
+  return value;
+}
+function select(value) {
+  let min;
+  for (OutIterations = 0; OutIterations < value.length - 1; OutIterations++) {
+    min = OutIterations;
+    for (
+      CountInIterations = OutIterations;
+      CountInIterations < value.length;
+      CountInIterations++
+    ) {
+      if (value[CountInIterations] < value[min]) {
+        min = CountInIterations;
+      }
+    }
+    let temp = value[OutIterations];
+    value[OutIterations] = value[min];
+    value[min] = temp;
+  }
+  return value;
+}
 
-// const arr = [1, 4, 2, 5, 3];
-// let min;
-// for (OutIterations = 0; OutIterations < arr.length - 1; OutIterations++) {
-//   min = OutIterations;
-//   for (
-//     CountInIterations = OutIterations;
-//     CountInIterations < arr.length;
-//     CountInIterations++
-//   ) {
-//     if (arr[CountInIterations] < arr[min]) {
-//       min = CountInIterations;
-//     }
-//   }
-//   let temp = arr[OutIterations];
-//   arr[OutIterations] = arr[min];
-//   arr[min] = temp;
-// }
-// console.log(arr);
-
-// const arr = [1, 4, 2, 5, 3, 8];
-// let j;
-// let value;
-// for (i = 1; i < arr.length; i++) {
-//   value = arr[i];
-//   for (j = i - 1; j >= 0 && arr[j] > value; j--) {
-//     arr[j + 1] = arr[j];
-//   }
-//   arr[j + 1] = value;
-// }
-// console.log(arr);
+function insert(value) {
+  let j;
+  let value1;
+  for (i = 1; i < value.length; i++) {
+    value1 = value[i];
+    for (j = i - 1; j >= 0 && value[j] > value1; j--) {
+      value[j + 1] = value[j];
+    }
+    value[j + 1] = value1;
+  }
+  return value;
+}
